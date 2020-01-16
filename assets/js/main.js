5e860283f56eb1895aa3b88dff8b8b51c66e5cb1
Любомир Капов <kapoliub@Lubomirs-MacBook-Pro.local>
2020-01-16 08:57:52 +0200

    added difficult levels and their choose interface

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+    let isStarted = false;
     function shuffle(arr){
         let j, temp;
         for(let i = arr.length - 1; i > 0; i--){
@@ -22,9 +23,16 @@ $(function(){
         return arr;
     }
 
-    fillField(4);
 
     function fillField(sideSize){
+        for(let i = 0; i < sideSize; i++){
+            $('#play-field').append(`<tr></tr>`) 
+        }
+        $('tr').each(function(){
+            for(let i = 0; i < sideSize; i++){
+                $(this).append(`<td></td>`)
+            }
+        })
         let arr = getArray(sideSize);
 
         $("td").each(function(i){
@@ -34,44 +42,110 @@ $(function(){
 
     function openCards(){
         let firstCard = {
-            value: '',
-            location: ''
+            id: '',
+            data: ''
         }
 
         let secondCard = {
-            value: '',
-            location: ''
+            id: '',
+            data: ''
         }
-        $(document).click(function(){
-            console.log($(this))
-        })
-        $("td").click(function (){
-            console.log(this)
-            if(firstCard.value == '' || secondCard.value == ''){
-                if(firstCard.value == ''){
-                    firstCard.value = $(this).text();
-                    firstCard.location = $(this);
-                    console.log(firstCard.value)
+        $("td img").click(function (){
+            if(firstCard.id == '' || secondCard.id == ''){
+                if(firstCard.id == ''){
+                    firstCard.id = $(this).attr('card-id')
+                    $(this).removeAttr('card-id')
+                    firstCard.data = $(this);
+                    console.log(firstCard.id)
                 }
-                else if(secondCard.value == ''){
-                    secondCard.value = $(this).text();
-                    secondCard.location = $(this);
-                    console.log(secondCard.value)
-                    if(firstCard.value === secondCard.value){
-                        firstCard.location.text('')
-                        secondCard.location.text('')
+                else if(secondCard.id == ''){
+                    secondCard.id = $(this).attr('card-id');
+                    secondCard.data = $(this);
+                    console.log(secondCard.id)
+                    if(firstCard.id === secondCard.id){
+                        firstCard.data.remove();
+                        secondCard.data.remove();
                         console.log('yes')
                     }
                     else{
-                        console.log('no')
+                        firstCard.data.attr('card-id', firstCard.id)
                     }
-                    firstCard.value = '';
-                    firstCard.location = '';
-                    secondCard.value = '';
-                    secondCard.location = '';
+                    firstCard.id = '';
+                    firstCard.data = '';
+                    secondCard.id = '';
+                    secondCard.data = '';
                 } 
             }
         });
     }
-});
 
+    // openCards()
+    // fillField(4);
+
+    
+    // pressStart()
+    
+
+    // function startBlinking() {
+    //     $("#start-screen span").toggle();
+    // }
+
+    // let blink = setInterval(startBlinking, 500);
+
+    function pressStart(){
+        // clearTimeout(blink);
+        $("#start-screen").hide();
+        $("#level-choose").show()
+        // chooseDifficult();
+        fillField(4)
+        openCards();
+    }
+    // fillField(6)
+    // chooseDifficult();
+    // openCards()
+
+    function chooseDifficult(){
+        let level;
+        $('#level-choose li').click(function(){
+            level = $(this).attr('id');
+            switch(level){
+                case 'level-1':
+                    fillField(4);
+                    $("#level-choose li").hide();
+                    $("#level-choose span").hide();
+                    break;
+                case 'level-2':
+                    fillField(6)
+                    $("#level-choose li").hide();
+                    $("#level-choose span").hide();
+                    break;
+                case 'level-3':
+                    fillField(8);
+                    $("#level-choose li").hide();
+                    $("#level-choose span").hide();
+                    break;
+                case 'level-4':
+                    fillField(10);
+                    $("#level-choose li").hide();
+                    $("#level-choose span").hide();
+                    break;
+            }
+        }); 
+        
+    }
+    
+
+    if(isStarted === false){
+        $(document).on('keyup',function(e){
+            if(e.keyCode === 13 || e.keyCode === 32){
+                pressStart();
+                isStarted = true;
+            }
+        });
+        
+        $("#start-screen").click(function(){
+            pressStart();
+            isStarted = true;
+        })
+    }
+});
